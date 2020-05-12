944ded463884c8aac2ef3a3c6325bb21a03d24b1
Daniel Howitt <daniel.howittcontact@gmail.com>
2020-05-12 14:36:40 +0100

    rails task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(new_task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    params[:completed] == '1' ? @task.completed = true : @task.completed = false
+    @task.update(task_params)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def new_task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
